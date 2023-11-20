17b75a63ff75a327c1e2ccf0322c91cd4c99c283
Louis Stafford <louiststafford@gmail.com>
2023-11-20 18:17:35 +0000

    Add breed fetch

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,17 @@
+const request = require('request');
 
+// console.log(process.argv);
+request(`https://api.thecatapi.com/v1/breeds/search?name=${process.argv[2]}`, (error, response, body) => {
+  // console.log(body)
+  // console.log('error:', error);
+  if (error) {
+    return console.log('Error!');
+  }
+  const data = JSON.parse(body);
+  // console.log(data);
+  // console.log(typeof data);
+  if (data[0] === undefined) {
+    return console.log('We could not find this breed.');
+  }
+  console.log(data[0]['description']);
+});
